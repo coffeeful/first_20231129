68a414267aba9246bf3d891a3f7a17985e3b59a9
coffeeful <dltjdgus03@gmail.com>
2023-11-29 15:41:01 +0900

    first commit

---
+++ src/a/First.java
@@ -0,0 +1,17 @@
+// 현재 클래스(자바파일)가 소속된 패키지의 이름
+package a;
+
+// 현재클래스의 이름
+public class First  {  // first calss 블록의 시작
+					// 보라색 굵은글씨는 바꾸면 안되는 부분(예약어)
+					//검은색 글씨는 맘대로 바꿔도 되는 부분
+		// main 메서드(프로그램의 시작 지점)
+	public static void main(String[] args) { // main 메서드 블록의 시작
+		// 실행할 내용이 작성되는 공간
+		// 텍스트 또는 변수값을 출력할 수 있는 println() 메서드
+		System.out.println("자바야 앞으로 친하게 지내자.."); 
+		// ; 세미콜론은 문장의 끝에 사용(문장의 끝을 알려주는의미)
+		
+	} // main 메서드 블록의 끝 
+
+} // first class 블록의 끝
